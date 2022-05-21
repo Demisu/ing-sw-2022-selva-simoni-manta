fb21400a9be75af5ddf1b6b42bf7a2f14350e412
Delath <simoni.dario1@gmail.com>
2022-05-21 18:08:44 +0200

    Pushing things that do not work

--- src/main/java/it/polimi/ingsw/client/GUI.java
+++ src/main/java/it/polimi/ingsw/client/GUI.java
@@ -3,32 +3,26 @@ package it.polimi.ingsw.client;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class GUI extends Application {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException  {
         launch(args);
     }
 
     @Override
     public void start(Stage primaryStage) {
-        primaryStage.setTitle("Hello World!");
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
+        Parent root = FXMLLoader.load(getClass().getResource("./gui.fxml"));
+        primaryStage.setTitle("Hello World");
+        primaryStage.setScene(new Scene(root, 300, 275));
         primaryStage.show();
     }
-
 }
