a758f1284479da8f30fa6c5b8e52974b79a9fa18
Ric <riccardo.manta@mail.polimi.it>
2022-05-28 14:56:26 +0200

    Created SceneController for switching scenes

---
+++ src/main/java/it/polimi/ingsw/client/gui/controllers/SceneController.java
@@ -0,0 +1,33 @@
+package it.polimi.ingsw.client.gui.controllers;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class SceneController {
+
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void switchToPlayerBoardScene(ActionEvent e) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/nickname.fxml"));
+        stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToSchoolBoardScene(ActionEvent e) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("/players.fxml"));
+        stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
