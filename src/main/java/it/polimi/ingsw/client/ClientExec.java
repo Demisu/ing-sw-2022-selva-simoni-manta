da76c802ea4ad98ad72dc43f9cee384034a3c1ad
Demis <demisselva@gmail.com>
2022-05-30 15:25:36 +0200

    Writing useless code to avoid responsibilities

--- src/main/java/it/polimi/ingsw/client/ClientExec.java
+++ src/main/java/it/polimi/ingsw/client/ClientExec.java
@@ -7,12 +7,22 @@ public class ClientExec {
     public static void main(String[] args) throws IOException {
 
         int port = 4567;
-
         Client client = new Client("127.0.0.1", port);
 
+        String ui;
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Select type:\nGUI or CLI?");
-        ClientController controller = new ClientController(client, scanner.nextLine().toUpperCase());
+
+        do {
+            System.out.println("Select type:\nCLI (1) or GUI (2)?");
+            ui = scanner.nextLine().toUpperCase();
+            if(ui.equals("1")) {
+                ui = "CLI";
+            } else if (ui.equals("2")) {
+                ui = "GUI";
+            }
+        }while (!(ui.equals("GUI") || ui.equals("CLI")));
+
+        ClientController controller = new ClientController(client, ui);
 
         try {
             client.initConnection();
