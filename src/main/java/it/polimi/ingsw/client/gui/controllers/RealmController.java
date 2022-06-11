f2c5705a6243714ff73ccf712d354c07d3f1a813
Delath <simoni.dario1@gmail.com>
2022-06-11 20:16:58 +0200

    Jesse we have to cook

--- src/main/java/it/polimi/ingsw/client/gui/controllers/RealmController.java
+++ src/main/java/it/polimi/ingsw/client/gui/controllers/RealmController.java
@@ -2,14 +2,22 @@ package it.polimi.ingsw.client.gui.controllers;
 
 import it.polimi.ingsw.client.gui.GUI;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
+import javafx.geometry.Insets;
 import javafx.scene.Node;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
 import javafx.scene.control.Button;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Text;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -80,6 +88,28 @@ public class RealmController implements GUIController {
         scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
+    }
+
+    public void openBag(MouseEvent e) throws IOException {
+        System.out.println("ciao");
+        stage = (Stage) ((Node) e.getSource()).getScene().getWindow();
+        final Stage dialog = new Stage();
+        Pane bagRoot = new Pane();
+        StackPane bagHolder = new StackPane();
+        Canvas canvas = new Canvas(2000,2000);
+        bagHolder.getChildren().add(canvas);
+        bagRoot.getChildren().add(bagHolder);
+        Scene bagScene = new Scene(bagRoot, 600, 400);
+        dialog.setScene(bagScene);
+        dialog.initModality(Modality.NONE);
+        dialog.initOwner(stage);
+        dialog.getIcons().add(new Image(getClass().getResourceAsStream("/assets/bag.png")));
+        VBox dialogVbox = new VBox(20);
+        dialogVbox.setBackground(new Background(new BackgroundFill(Color.BURLYWOOD, CornerRadii.EMPTY, Insets.EMPTY)));
+        dialogVbox.getChildren().add(new Text("\t [INFO TO BE ADDED..]"));
+        Scene dialogScene = new Scene(dialogVbox, 300, 200);
+        dialog.setScene(dialogScene);
+        dialog.show();
     }
 
     @Override
